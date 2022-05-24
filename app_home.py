3843363d79dc4061cdede81a08500f0a0661fb44
wngml317 <wngml317@naver.com>
2022-05-24 10:51:47 +0900

    add

--- app_home.py
+++ app_home.py
@@ -3,5 +3,4 @@ import streamlit as st
 def run_home() :
     st.subheader('이 앱은 고객 데이터와 자동차 구매 데이터에 대한 내용입니다.')
     st.write('왼쪽 사이드바에서 원하는 항목을 선택하세요.')
-
-    st.write('데이터 분석과 머신러닝 기능을 포함한 앱')
+    
